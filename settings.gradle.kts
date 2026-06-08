0f56780a87c68bb77ed1dd412e03222f85fbd4b4
Adam Zoeee <adamzoeee@foxmail.com>
2026-06-08 15:15:21 +0800

    fix: upgrade AGP/Kotlin/Hilt/Room to match Gradle 8.14.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
